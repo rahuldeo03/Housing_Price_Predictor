0b9ebda234012032c69ba06a6f2cfc1f10b10359
Rahul03 <Rahul03@ad.infosys.com>
2020-09-08 19:18:14 +0530

    checkin

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from flask import Flask, render_template, request
-import jsonify
-import requests
 import pickle
-import numpy as np
-import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('housing_price.pkl', 'rb'))
@@ -15,18 +11,11 @@ def Home():
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 def predict():
-    Fuel_Type_Diesel=0
     if request.method == 'POST':
         rooms = int(request.form['rooms'])
         distance=float(request.form['distance'])
         prediction=model.predict([[rooms,distance]])
         output=round(prediction[0],2)
-        if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
-        else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
-    else:
-        return render_template('index.html')
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this phone")
         else:
